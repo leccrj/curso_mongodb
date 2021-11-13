b8a71d668f5fdcaecb2adb23aa4dde690efc9b06
Luiz Eduardo <leccrj@gmail.com>
2021-11-13 02:06:37 -0300

    Primeiro commit do cuso mongodb

---
+++ src/main/java/com/dudu/cursomongodb/CursoMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.dudu.cursomongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dudu/cursomongodb/CursoMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudu.cursomongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
