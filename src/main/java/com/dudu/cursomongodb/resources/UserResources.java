f157b2427b3a117d2c38b616a4553c4db23d0bd9
Luiz Eduardo <leccrj@gmail.com>
2021-11-14 18:28:45 -0300

    Obtendo um usuario po id

--- src/main/java/com/dudu/cursomongodb/resources/UserResources.java
+++ src/main/java/com/dudu/cursomongodb/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +24,21 @@ public class UserResources {
 	@RequestMapping(method = RequestMethod.GET)
 	//@GetMapping (maneira alternativa)
 	public ResponseEntity<List<UserDTO>> findAll(){
+		
 		List<User> list = service.findAll();
+		
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
 		return ResponseEntity.ok().body(listDto);
+		
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(obj));
+		
 	}
 }
