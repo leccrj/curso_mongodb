6d884aa2329ad3830fd83c859cff941ed6dd8ff6
Luiz Eduardo <leccrj@gmail.com>
2021-11-14 19:25:40 -0300

    Insercao de usuario com POST

--- src/main/java/com/dudu/cursomongodb/resources/UserResources.java
+++ src/main/java/com/dudu/cursomongodb/resources/UserResources.java
@@ -1,14 +1,18 @@
 package com.dudu.cursomongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dudu.cursomongodb.domain.User;
 import com.dudu.cursomongodb.dto.UserDTO;
@@ -39,6 +43,20 @@ public class UserResources {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	//@PostMapping (maneira alternativa)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		
+		User obj = service.fromDTO(objDto);
+		
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 		
 	}
 }
