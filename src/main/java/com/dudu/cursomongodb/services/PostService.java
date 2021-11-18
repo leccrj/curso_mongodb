f6d939a73f211dd3a5d3d540062473d6d1e5d431
Luiz Eduardo <leccrj@gmail.com>
2021-11-17 23:02:59 -0300

    Obtendo um post por id

---
+++ src/main/java/com/dudu/cursomongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.dudu.cursomongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dudu.cursomongodb.domain.Post;
+import com.dudu.cursomongodb.repository.PostRepository;
+import com.dudu.cursomongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired //mecanismo de injeção de depencia automática do springboot
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}	
+}
