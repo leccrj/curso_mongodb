808c2f76fd6f845b2e07ad0866edc915cae1076a
Luiz Eduardo <leccrj@gmail.com>
2021-11-17 22:49:32 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/dudu/cursomongodb/resources/UserResources.java
+++ src/main/java/com/dudu/cursomongodb/resources/UserResources.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.dudu.cursomongodb.domain.Post;
 import com.dudu.cursomongodb.domain.User;
 import com.dudu.cursomongodb.dto.UserDTO;
 import com.dudu.cursomongodb.services.UserService;
@@ -79,6 +79,15 @@ public class UserResources {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 		
 	}
 }
