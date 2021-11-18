f3a5ebc74eb97ebf70dcdc6fe8d163e3dbf5a10b
Luiz Eduardo <leccrj@gmail.com>
2021-11-18 20:56:56 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/dudu/cursomongodb/config/Instantiation.java
+++ src/main/java/com/dudu/cursomongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.dudu.cursomongodb.domain.Post;
 import com.dudu.cursomongodb.domain.User;
 import com.dudu.cursomongodb.dto.AuthorDTO;
+import com.dudu.cursomongodb.dto.CommentDTO;
 import com.dudu.cursomongodb.repository.PostRepository;
 import com.dudu.cursomongodb.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null,sdf.parse("16/11/2021"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("17/11/2021"),"Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("17/11/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("18/11/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("18/11/2021"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepo.saveAll(Arrays.asList(post1,post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
