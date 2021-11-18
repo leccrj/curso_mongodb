6a20c3956e54ce371bee5f4ff11443d227c6b288
Luiz Eduardo <leccrj@gmail.com>
2021-11-17 22:20:09 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/dudu/cursomongodb/config/Instantiation.java
+++ src/main/java/com/dudu/cursomongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.dudu.cursomongodb.domain.Post;
 import com.dudu.cursomongodb.domain.User;
+import com.dudu.cursomongodb.dto.AuthorDTO;
 import com.dudu.cursomongodb.repository.PostRepository;
 import com.dudu.cursomongodb.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null,"Alex Green","alex@gmail.com");
 		User bob = new User(null,"Bob Grey","bob@gmail.com");
 		
-		Post post1 = new Post(null,sdf.parse("16/11/2021"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",maria);
-		Post post2 = new Post(null,sdf.parse("17/11/2021"),"Bom dia","Acordei feliz hoje!",maria);
-				
 		userRepo.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null,sdf.parse("16/11/2021"),"Partiu viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("17/11/2021"),"Bom dia","Acordei feliz hoje!",new AuthorDTO(maria));
+				
+		
 		postRepo.saveAll(Arrays.asList(post1,post2));
 	}
 
