d36be9034769eaae18c9b5fcf00f03db3c9b8449
Luiz Eduardo <leccrj@gmail.com>
2021-11-18 21:48:49 -0300

    Consulta simple com @Query

--- src/main/java/com/dudu/cursomongodb/repository/PostRepository.java
+++ src/main/java/com/dudu/cursomongodb/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.dudu.cursomongodb.repository;
 
 import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dudu.cursomongodb.domain.Post;
@@ -9,5 +11,8 @@ import com.dudu.cursomongodb.domain.Post;
 @Repository //spring data
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContaining(String text);
 }
