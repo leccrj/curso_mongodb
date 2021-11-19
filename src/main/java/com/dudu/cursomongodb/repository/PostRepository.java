0f9bfa89f6b42c5c3aae7f1e143a406d7bce37b4
Luiz Eduardo <leccrj@gmail.com>
2021-11-18 21:35:06 -0300

    Consulta simples com query methods

--- src/main/java/com/dudu/cursomongodb/repository/PostRepository.java
+++ src/main/java/com/dudu/cursomongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.dudu.cursomongodb.repository;
 
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import com.dudu.cursomongodb.domain.Post;
 @Repository //spring data
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContaining(String text);
 }
--- src/main/java/com/dudu/cursomongodb/resources/PostResources.java
+++ src/main/java/com/dudu/cursomongodb/resources/PostResources.java
@@ -1,13 +1,17 @@
 package com.dudu.cursomongodb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dudu.cursomongodb.domain.Post;
+import com.dudu.cursomongodb.resources.util.URL;
 import com.dudu.cursomongodb.services.PostService;
 
 @RestController
@@ -20,10 +24,17 @@ public class PostResources {
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		
-		Post obj = service.findById(id);
-		
+		Post obj = service.findById(id);	
 		return ResponseEntity.ok().body(obj);
 		
 	}
 	
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+		
+	}
 }
